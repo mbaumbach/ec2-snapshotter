9fa35b7ce093a2757ceae8815934a1f5f9ec61b3
Marc Baumbach <marc.baumbach@bronto.com>
2015-09-05 21:56:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# EC2 Snapshotter
+
+This program allows for a simple mechanism to snapshot all volumes in your EC2 environment. 
+This can run on any server, but will backup any volumes that have a tag with a key of `Backup` 
+and a value of `True`.
+
+The program will also automatically clean up all snapshots it has created that are older than 
+one month. It does this by only deleting volumes with the tag key of `Snapshotter` and a value 
+of `True` which have a snapshot start time of over one month ago.
+
+All actions performed by this program are logged to a `snapshotter.log` file. This file is 
+automatically rolled and a single rolled file is kept at all times.
+
+## System Requirements
+
+You must have Java 8 on your system classpath to run this program.
+
+## Running the program
+
+`java -jar ec2-snapshotter.jar [AWS-Key AWS-Secret]`
+
+The AWS key and secret are optional and when omitted, the program will attempt to use the 
+EC2 instance profile's credentials. Make sure you have the appropriate permissions on your 
+IAM role to allow listing volumes, creating and deleting snapshots, and tagging resources.
+
+## Scheduling the Backup
+
+On Linux, you can use `cron` to perform scheduled backups. The following crontab entry will 
+run every night at midnight server local time.
+
+`0 * * * * java -jar /path/to/program/ec2-snapshotter.jar`
+
+## License
+
+The EC2 Snapshotter is licensed under the Apache 2.0 License.
---
+++ src/main/java/com/marcbaumbach/ec2/snapshotter/Snapshotter.java
@@ -0,0 +1,106 @@
+package com.marcbaumbach.ec2.snapshotter;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.internal.StaticCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.CreateSnapshotRequest;
+import com.amazonaws.services.ec2.model.CreateTagsRequest;
+import com.amazonaws.services.ec2.model.DeleteSnapshotRequest;
+import com.amazonaws.services.ec2.model.DescribeSnapshotsRequest;
+import com.amazonaws.services.ec2.model.DescribeSnapshotsResult;
+import com.amazonaws.services.ec2.model.DescribeVolumesRequest;
+import com.amazonaws.services.ec2.model.DescribeVolumesResult;
+import com.amazonaws.services.ec2.model.Filter;
+import com.amazonaws.services.ec2.model.Snapshot;
+import com.amazonaws.services.ec2.model.Tag;
+import com.amazonaws.services.ec2.model.Volume;
+
+public class Snapshotter {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Snapshotter.class);
+	
+	private static final String SNAPSHOTTER_TAG_KEY = "SnapshotterBackup";
+	private static final String FILTER_TAG_BACKUP = "tag:Backup";
+	private static final String FILTER_TAG_SNAPSHOTTER = "tag:" + SNAPSHOTTER_TAG_KEY;
+	private static final String TRUE_VALUE = "True";
+	private static final String TAG_NAME = "Name";
+	private static final int NUMBER_OF_MONTHS = 1;
+	
+	private static AWSCredentialsProvider getCredentials(String[] args) {
+		AWSCredentialsProvider provider = null;
+		if (args.length == 2) {
+			provider = new StaticCredentialsProvider(new BasicAWSCredentials(args[0], args[1]));
+		} else {
+			logger.info("No credentials found in arguments, using instance profile");
+			provider = new InstanceProfileCredentialsProvider();
+		}
+		return provider;
+	}
+	
+	private static CreateSnapshotRequest createSnapshotRequest(Volume volume) {
+		String description = volume.getTags().stream()
+				.filter(t -> t.getKey().equals(TAG_NAME))
+				.findFirst()
+				.map(t -> t.getValue())
+				.orElse(volume.getVolumeId());
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+		description += " - " + format.format(new Date());
+		logger.info("Creating snapshot {} for volume {}", description, volume.getVolumeId());
+		return new CreateSnapshotRequest(volume.getVolumeId(), description);
+	}
+	
+	private static DeleteSnapshotRequest deleteSnapshotRequest(Snapshot snapshot) {
+		logger.info("Deleting snapshot {} for volume {}", snapshot.getDescription(), snapshot.getVolumeId());
+		return new DeleteSnapshotRequest(snapshot.getSnapshotId());
+	}
+	
+	public static void main(String[] args) {
+		long start = System.nanoTime();
+		logger.info("Creating snapshots...");
+		AmazonEC2 ec2Client = new AmazonEC2Client(getCredentials(args));
+		DescribeVolumesRequest describeVolumes = new DescribeVolumesRequest()
+				.withFilters(new Filter(FILTER_TAG_BACKUP, Arrays.asList(TRUE_VALUE)));
+		DescribeVolumesResult volumesResult = ec2Client.describeVolumes(describeVolumes);
+		List<String> snapshotIds = volumesResult.getVolumes().stream()
+			.map(Snapshotter::createSnapshotRequest)
+			.map(ec2Client::createSnapshot)
+			.map(csr -> csr.getSnapshot().getSnapshotId())
+			.collect(Collectors.toList());
+		logger.info("Completed snapshots in {}s", (TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start)));
+		
+		start = System.nanoTime();
+		logger.info("Tagging snapshots...");
+		ec2Client.createTags(new CreateTagsRequest(snapshotIds, Arrays.asList(new Tag(SNAPSHOTTER_TAG_KEY, TRUE_VALUE))));
+		logger.info("Completed tagging snapshots in {}s", (TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start)));
+		
+		start = System.nanoTime();
+		logger.info("Cleaning up snapshots older than {} month(s)...", NUMBER_OF_MONTHS);
+		DescribeSnapshotsRequest describeSnapshots = new DescribeSnapshotsRequest()
+				.withFilters(new Filter(FILTER_TAG_SNAPSHOTTER, Arrays.asList(TRUE_VALUE)));
+		DescribeSnapshotsResult snapshotsResult = ec2Client.describeSnapshots(describeSnapshots);
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date());
+		calendar.add(Calendar.MONTH, -NUMBER_OF_MONTHS);
+		Date cutoff = calendar.getTime();
+		snapshotsResult.getSnapshots().stream()
+			.filter(s -> s.getStartTime().before(cutoff))
+			.map(Snapshotter::deleteSnapshotRequest)
+			.forEach(ec2Client::deleteSnapshot);
+		logger.info("Completed snapshot cleanup in {}s", (TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start)));
+	}
+
+}
